ae9be4a7c0ddb5e74717d3c3e387e0954f560ee7
Mehmet Gökay Korik <44945301+Korikk@users.noreply.github.com>
2024-01-24 15:46:44 +0300

    Add files via upload

---
+++ helpers.py
@@ -0,0 +1,193 @@
+import mlxtend
+from mlxtend.frequent_patterns import apriori, association_rules
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def check_df(dataframe):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(3))
+    print("##################### Tail #####################")
+    print(dataframe.tail(3))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+
+def outlier_thresholds(dataframe, variable):
+    quartile1 = dataframe[variable].quantile(0.01)
+    quartile3 = dataframe[variable].quantile(0.99)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    # dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    """
+    grab_col_names for given dataframe
+    :param dataframe:
+    :param cat_th:
+    :param car_th:
+    :return:
+    """
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
+                   dataframe[col].dtypes != "O"]
+
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and
+                   dataframe[col].dtypes == "O"]
+
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+
+    # cat_cols + num_cols + cat_but_car = değişken sayısı.
+    # num_but_cat cat_cols'un içerisinde zaten.
+    # dolayısıyla tüm şu 3 liste ile tüm değişkenler seçilmiş olacaktır: cat_cols + num_cols + cat_but_car
+    # num_but_cat sadece raporlama için verilmiştir.
+
+    return cat_cols, cat_but_car, num_cols, num_but_cat
+
+
+#############################################
+# CATEGORICAL
+#############################################
+
+def cat_summary(dataframe, col_name, plot=False):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+
+    if plot:
+        sns.countplot(x=dataframe[col_name], data=dataframe)
+        plt.show()
+
+
+#############################################
+# NUMERICAL
+#############################################
+
+def num_summary(dataframe, numerical_col, plot=False):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+    if plot:
+        dataframe[numerical_col].hist(bins=20)
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show()
+
+
+#############################################
+# TARGET
+#############################################
+
+def target_summary_with_cat(dataframe, target, categorical_col):
+    print(pd.DataFrame({"TARGET_MEAN": dataframe.groupby(categorical_col)[target].mean()}), end="\n\n\n")
+
+
+def target_summary_with_num(dataframe, target, numerical_col):
+    print(dataframe.groupby(target).agg({numerical_col: "mean"}), end="\n\n\n")
+
+
+###########################################################################################
+
+
+def crm_data_prep(dataframe):
+    dataframe.dropna(axis=0, inplace=True)
+    dataframe = dataframe[~dataframe["Invoice"].str.contains("C", na=False)]
+    dataframe = dataframe[dataframe["Quantity"] > 0]
+    replace_with_thresholds(dataframe, "Quantity")
+    replace_with_thresholds(dataframe, "Price")
+    dataframe["TotalPrice"] = dataframe["Quantity"] * dataframe["Price"]
+    return dataframe
+
+
+def create_invoice_product_df(dataframe):
+    return dataframe.groupby(['Invoice', 'StockCode'])['Quantity'].sum().unstack().fillna(0). \
+        applymap(lambda x: 1 if x > 0 else 0)
+
+
+#############################################################################################
+
+def plot_importance(model, features, num=len(x), save=False):
+    feature_imp = pd.DataFrame({'Value': model.feature_importances_, 'Feature': features.columns})
+    plt.figure(figsize=(10, 10))
+    sns.set(font_scale=1)
+    sns.barplot(x="Value", y="Feature", data=feature_imp.sort_values(by="Value",
+                                                                     ascending=False)[0:num])
+    plt.title('Features')
+    plt.tight_layout()
+    plt.show()
+    if save:
+        plt.savefig('importances.png')
+
+
+#################################################################################################
+
+
+def content_based_recommender(title, cosine_sim, dataframe):
+    # index'leri olusturma
+    dataframe = dataframe[~dataframe["title"].isna()]
+    indices = pd.Series(dataframe.index, index=dataframe['title'])
+    indices = indices[~indices.index.duplicated(keep='last')]
+    # title'ın index'ini yakalama
+    movie_index = indices[title]
+    # title'a gore benzerlik skorlarını hesapalama
+    similarity_scores = pd.DataFrame(cosine_sim[movie_index], columns=["score"])
+    # kendisi haric ilk 10 filmi getirme
+    movie_indices = similarity_scores.sort_values("score", ascending=False)[1:11].index
+    return dataframe['title'].iloc[movie_indices]
+
+
+#################################################################################################
+
+
+def calculate_cosine_sim(dataframe):
+    tfidf = TfidfVectorizer(stop_words='english')
+    dataframe['overview'] = dataframe['overview'].fillna('')
+    tfidf_matrix = tfidf.fit_transform(dataframe['overview'])
+    cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
+    return cosine_sim
+
+
+##############################################################################################
+
+
+def create_user_movie_df():
+    import pandas as pd
+    movie = pd.read_csv('datasets/movie_lens_dataset/movie.csv')
+    rating = pd.read_csv('datasets/movie_lens_dataset/rating.csv')
+    df = movie.merge(rating, how="left", on="movieId")
+    df['title'] = df.title.str.replace('(\(\d\d\d\d\))', '')
+    df['title'] = df['title'].apply(lambda x: x.strip())
+    a = pd.DataFrame(df["title"].value_counts())
+    rare_movies = a[a["title"] <= 1000].index
+    common_movies = df[~df["title"].isin(rare_movies)]
+    user_movie_df = common_movies.pivot_table(index=["userId"], columns=["title"], values="rating")
+    return user_movie_df
+
+
+##########################################################################################################
